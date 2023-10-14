9da82f0a142a83cd6538566bd62701f02b68ab15
yellabindusree <yellabindusree@gmail.com>
2023-10-14 08:36:33 +0530

    commit

---
+++ task4/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+
+import Screen from './components/Appscreen';
+
+function App() {
+  return (
+    <div className='bg-dark container-fluid'>
+      <Screen />
+    </div>
+  );
+}
+
+export default App;
